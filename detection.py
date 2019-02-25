7cbfbcf8581da502384d93da1c00f5b0fac1aa84
wei-x15 <wei-x15@mails.tsinghua.edu.cn>
2019-02-25 10:46:51 +0800

    refactor detection code into Detector class

--- detection.py
+++ detection.py
@@ -3,36 +3,41 @@ import torch
 
 from bbox import threshold_confidence, NMS
 from darknet_parsing import parse_cfg_file, parse_darknet, parse_weights_file
-from letterbox import letterbox_image, inv_letterbox_image
+from letterbox import letterbox_image, inv_letterbox_bbox
 from util import load_classes, color_map, cvmat_to_tensor, draw_detections
 
 
-def detect(net, tensor):
-    net.eval()
-    with torch.no_grad():
-        output = net(tensor.unsqueeze(0)).data
+class Detecter:
+    classes = load_classes('data/coco.names')
+    cmap = color_map(len(classes))
+
+    def __init__(self, inp_dim=None):
+        # Set up the neural network
+        self.net_info, self.net = parse_darknet(parse_cfg_file('cfg/yolov3.cfg'))
+        parse_weights_file(self.net, 'weights/yolov3.weights')
+        self.net.cuda().eval()
+        print("Network successfully loaded")
+
+        self.inp_dim = inp_dim if inp_dim is not None else self.net_info["inp_dim"][::-1]
+
+    def detect(self, cvmat):
+        img = letterbox_image(cvmat, self.inp_dim)
+        tensor = cvmat_to_tensor(img).unsqueeze(0).cuda()
+        with torch.no_grad():
+            output = self.net(tensor).data
         output = threshold_confidence(output)
-        return NMS(output)[0]
+        output = NMS(output)[0]
+        inv_letterbox_bbox(output[0], self.inp_dim, cvmat.shape[:2])
+        return tuple(y.cpu() for y in output)
 
 
 if __name__ == '__main__':
-    # Set up the neural network
-    net_info, net = parse_darknet(parse_cfg_file('cfg/yolov3.cfg'))
-    parse_weights_file(net, 'weights/yolov3.weights')
-    print("Network successfully loaded")
+    detecter = Detecter()
 
-    inp_dim = net_info["inp_dim"][::-1]
+    img = cv2.imread('imgs/dog-cycle-car.png')
+    output = detecter.detect(img)
 
-    orig_img = cv2.imread('imgs/dog-cycle-car.jpg')
-    img = letterbox_image(orig_img, inp_dim)
-
-    output = detect(net, cvmat_to_tensor(img))
-
-    classes = load_classes('data/coco.names')
-    cmap = color_map(len(classes))
-    draw_detections(img, output, classes, cmap)
-
-    img = inv_letterbox_image(img, orig_img.shape[:-1])
+    draw_detections(img, output, Detecter.classes, Detecter.cmap)
 
     cv2.imshow('image', img)
     cv2.waitKey(0)
--- video_play.py
+++ video_play.py
@@ -1,46 +1,30 @@
 import cv2
 import torch
 
-from darknet_parsing import parse_cfg_file, parse_darknet, parse_weights_file
-from detection import detect
-from letterbox import letterbox_image, inv_letterbox_bbox
+from detection import Detecter
 from sort import Sort
-from util import load_classes, color_map, DurationTimer, draw_text, cvmat_to_tensor, iterate_video, \
-    draw_trackers
+from util import DurationTimer, draw_text, iterate_video, draw_trackers
 
 if __name__ == '__main__':
-    # Set up the neural network
-    net_info, net = parse_darknet(parse_cfg_file('cfg/yolov3.cfg'))
-    parse_weights_file(net, 'weights/yolov3.weights')
-    print("Network successfully loaded")
-
-    # inp_dim = net_info["inp_dim"][::-1]
-
     cap = cv2.VideoCapture('/home/wei-x15/Downloads/TownCentreXVID.avi')
     orig_dim = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    inp_dim = tuple((x // 4 // 2 ** 5 + 1) * 2 ** 5 for x in orig_dim)
 
-    classes = load_classes('data/coco.names')
-    cmap = color_map(len(classes))
+    inp_dim = tuple((x // 4 // 2 ** 5 + 1) * 2 ** 5 for x in orig_dim)
+    detecter = Detecter(inp_dim)
 
     tracker = Sort()
 
-    net.cuda().eval()
-    with torch.no_grad():
-        for frame in iterate_video(cap):
-            with DurationTimer() as d:
-                output = detect(net, cvmat_to_tensor(letterbox_image(frame, inp_dim)).cuda())
-                output = tuple(y[output[1] == classes.index('person')] for y in output)  # Select persons
-                inv_letterbox_bbox(output[0], inp_dim, orig_dim)
-                output = tuple(y.cpu() for y in output)
-                trks = tracker.update(torch.cat((output[0], output[2].unsqueeze(1)), dim=1).numpy())
+    for frame in iterate_video(cap):
+        with DurationTimer() as d:
+            output = detecter.detect(frame)
+            output = tuple(y[output[1] == Detecter.classes.index('person')] for y in output)  # Select persons
+            trks = tracker.update(torch.cat((output[0], output[2].unsqueeze(1)), dim=1).numpy())
 
-            # draw_detections(frame, output, classes, cmap)
-            draw_trackers(frame, trks)
-            draw_text(frame, f'FPS: {int(1 / d.duration)}', [255, 255, 255], upper_right=(frame.shape[1] - 1, 0))
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        draw_trackers(frame, trks)
+        draw_text(frame, f'FPS: {int(1 / d.duration)}', [255, 255, 255], upper_right=(frame.shape[1] - 1, 0))
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     cap.release()
     cv2.destroyAllWindows()
